04c054b58c5529d8c89b9574cb6417210a4a746e
Åsa Jonsson <Asa.Jonsson@lexicon.se>
2022-09-06 11:58:33 +0200

    Add project files.

---
+++ StartUp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace StartUp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // if int or a string is null to the program use the ? behind the variable type
+            //string? message = "Alex";
+            //Console.WriteLine(message);
+
+            //int numA = 2;
+            //int numB = 2;
+            //bool isEqual = numA == numB;
+            //Console.WriteLine(isEqual);
+            //Console.ReadKey();
+
+            // comment out ctrl+k+c
+            // comment In ctrl+k+U
+            //int number = Convert.ToInt32(Console.ReadLine());
+            // number = 6;
+            //while (number > 0)
+            //{
+            //    Console.WriteLine(number);
+            //    number--;
+            //}
+            //do
+            //{
+            //    Console.WriteLine(number);
+            //    number--;
+            //}
+            //while (number > 0);
+
+            //for(int i = 0; i < 100; i++)
+            //{
+            //    Console.WriteLine("written 10 time" + i);
+
+            //}
+        }// end of Main
+    }// end of program
+}// end of namespace
